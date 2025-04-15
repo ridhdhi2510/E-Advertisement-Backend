48d3428ced67683c92d6ad178483bd6f26220305
Krishna Gojiya <krishnagojiya2206@gmail.com>
2025-04-16 02:36:37 +0530

    bank details by userId

--- src/controllers/BankDetailsAgencyController.js
+++ src/controllers/BankDetailsAgencyController.js
@@ -20,6 +20,22 @@ const addBankDetails = async(req,res)=>{
     }
 }
 
+const getBankDetailsbyuserId = async(req,res)=>{
+    try{
+        const showBankDetails = await bankdetailsagencyModel.findOne({ userId: req.params.userId }).populate("userId");
+        res.status(200).json({
+            message: "Bank details of agency fetched successfully...",
+            data: showBankDetails
+        }) 
+    }
+    catch(err){
+        res.status(500).json({
+            message: err.message
+        })
+    }
+}
+
 module.exports = {
-    addBankDetails
+    addBankDetails,
+    getBankDetailsbyuserId
 }
--- src/routes/BankDetailsAgencyRoutes.js
+++ src/routes/BankDetailsAgencyRoutes.js
@@ -2,5 +2,6 @@ const routes = require("express").Router();
 const bankdetailsagencyController = require("../controllers/BankDetailsAgencyController");
 
 routes.post("/add",bankdetailsagencyController.addBankDetails);
+routes.get("/getbyuserId/:userId",bankdetailsagencyController.getBankDetailsbyuserId);
 
 module.exports = routes;
